dc15b8d28d3d9699541765c06612d823f6199b82
jorawaraksk <jorawarzu@proton.me>
2025-07-26 00:49:00 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,7 +3,7 @@ from decouple import config
 try:
     APP_ID = config("APP_ID", "16732227")
     API_HASH = config("API_HASH", "8b5594ad7ad37f3a0a7ddbfb3963bb51")
-    BOT_TOKEN = config("BOT_TOKEN", "your-bot-token")
+    BOT_TOKEN = config("BOT_TOKEN", "7141984677:AAFztMNvuW-CYbsM1Ydq0zTZ6OQumWSEHTs")
     DEV = 5868426717
     OWNER = config("OWNER", "5868426717").split()
     PUBLIC = config("PUBLIC", default=True, cast=bool)
