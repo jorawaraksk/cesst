da99579e1aed07b5d194184854da1a014230d00f
jorawaraksk <jorawarzu@proton.me>
2025-07-26 03:25:18 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,7 +5,7 @@ try:
     API_HASH = config("API_HASH", "8b5594ad7ad37f3a0a7ddbfb3963bb51")
     BOT_TOKEN = config("BOT_TOKEN", "6504389382:AAFGj5_NPwTDcn0_FPXSU8_gBcYKRxmAVIc")
     DEV = 5868426717
-    OWNER = config("OWNER", "5868426717").split()
+    OWNER = config("OWNER", default="5868426717").split()
     PUBLIC = config("PUBLIC", default=True, cast=bool)
     ffmpegcode = ["-preset veryfast -c:v libx265 -b:a 64k -crf 38 -map 0 -c:s copy"]
     THUMB = config("THUMB", "https://static7.depositphotos.com/1009183/713/i/950/depositphotos_7132926-stock-photo-beautiful-tree-in-the-sunset.jpg")
